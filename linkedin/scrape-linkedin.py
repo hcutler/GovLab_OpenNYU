f3d7b297dbef05868cfb33ec6d9dc26f319f0f15
hcutler <hcutler@seas.upenn.edu>
2015-07-02 17:37:39 -0400

    still not working but added code from online documentation on github page

--- linkedin/scrape-linkedin.py
+++ linkedin/scrape-linkedin.py
@@ -6,6 +6,10 @@ from linkedin import linkedin
 from oauthlib import *
 from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
                                PERMISSIONS)
+from linkedin import server
+
+
+# docs: http://ozgur.github.io/python-linkedin/
 
 API_KEY = '77p81cbtycxb3s' #CONSUMER_KEY
 API_SECRET = 'NllPQrOxRDcXUGK6' #CONSUMER_SECRET
@@ -15,17 +19,25 @@ API_SECRET = 'NllPQrOxRDcXUGK6' #CONSUMER_SECRET
 RETURN_URL = 'http://localhost:8000'
 
 authentication = linkedin.LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
-# print authentication.authorization_url  # open this url on your browser
-
-# Pass it in to the app...
-application = linkedin.LinkedInApplication(authentication)
-
-# Use the app....
-# g = application.get_profile()
-g = application.r_basicprofile()
-print 'successful\n'
-print g
+print authentication.authorization_url  # open this url on your browser
+application = linkedin.LinkedInApplication(authentication) # Pass it in to the app...
 
 
+authentication.authorization_code = 'AQTXrv3Pe1iWS0EQvLg0NJA8ju_XuiadXACqHennhWih7iRyDSzAm5jaf3R7I8'
+authentication.get_access_token()
 
-# r_basicprofile
+
+application = server.quick_api(KEY, SECRET)
+
+# # Use the app....
+# gp = application.get_profile()
+
+
+gp = application.get_profile(selectors=['id', 'first-name', 'last-name', 'location', 'distance', 'num-connections', 'skills', 'educations'])
+# application.get_connections(selectors=['headline', 'first-name', 'last-name'], params={'start':10, 'count':5})
+# application.search_profile(selectors=[{'people': ['first-name', 'last-name']}], params={'keywords': 'apple microsoft'})
+# application.search_company(selectors=[{'companies': ['name', 'universal-name', 'website-url']}], params={'keywords': 'apple microsoft'})
+
+
+# g = application.r_basicprofile()
+print gp
