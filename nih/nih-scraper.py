685e12cae28dbc2ea18a5bd0f5f6499c951f0586
hcutler <hcutler@seas.upenn.edu>
2015-07-17 16:47:34 -0400

    adding files

---
+++ nih/nih-scraper.py
@@ -0,0 +1,18 @@
+from lxml import etree
+import lxml
+import os
+from os.path import join, getsize
+from xml.etree.ElementTree import ElementTree
+import csv
+from csv import DictWriter
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+#initialize arrays
+
+#applicationid
+#
