e58efd1be8d393f946681e4f174c5a6d08aee299
hcutler <hcutler@seas.upenn.edu>
2015-07-20 12:57:29 -0400

    copied css file and properly load onto web

--- expert-net-mockup-1/run.py
+++ expert-net-mockup-1/run.py
@@ -1,16 +1,39 @@
-from flask import Flask, render_template, abort
+from jinja2 import Environment, FileSystemLoader
+from yaml import load, Loader
+import shutil
 
-app = Flask(__name__)
+import os
 
-# establishing the route /, which renders the template 
-# template.html via the function render_template()
-@app.route("/")
+env = Environment(loader=FileSystemLoader("templates"))
 
+try:
+	os.mkdir('output')
+except OSError:
+	pass
 
 # function must have a template name
-def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def render():
+	# data_file = open("data.yaml", 'r')
+	# render_template('template.html', **data)
+	# data_file.close()
 
+	with open("data.yaml", 'r') as data_file:
+		data = load(data_file, Loader=Loader)
+
+	template = env.get_template("index.html")
+	profile = template.render(data)
+
+	with open("output/sample.html", 'w') as output_file:
+		output_file.write(profile)
+
+def copy_css():
+	source = os.getcwd() + "/"
+	dest = "/Users/hannahcutler/Documents/Dev/ENV/GovLab_OpenNYU/expert-net-mockup-1/output/"
+	for basename in os.listdir(source):
+		if basename.endswith(".css"):
+			shutil.copy2(basename,dest)
+			
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    render()
+    copy_css()
